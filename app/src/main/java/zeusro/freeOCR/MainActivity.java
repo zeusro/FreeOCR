23323476002ff739f2cfd9c5778e4ef80af6b4f3
Zeusro <692486802@qq.com>
2016-02-12 22:46:14 +0800

    明天约妹子去看同人祭啦,顺便完成了右下角的 FloatingActionButton

--- app/src/main/java/zeusro/freeOCR/MainActivity.java
+++ app/src/main/java/zeusro/freeOCR/MainActivity.java
@@ -2,11 +2,8 @@ package zeusro.freeOCR;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,14 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
 
         if (getString(R.string.subscription_key).startsWith("Please")) {
